c15b067aa8dde736fc0c22fb07b933b1a8232f06
Andrew Horn <arhorn@smcm.edu>
2017-11-02 18:06:33 +0700

    Filled out functions in convertHandler.js

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -9,42 +9,77 @@
 function ConvertHandler() {
   
   this.getNum = function(input) {
-    var result;
-    
-    return result;
+    var re = /[\d./]+/;
+    return re.exec(input); 
   };
   
   this.getUnit = function(input) {
-    var result;
-    
-    return result;
+    var re = /\w+/;
+    return re.exec(input);
   };
   
   this.getReturnUnit = function(initUnit) {
-    var result;
-    
-    return result;
+    switch (initUnit) {
+      case 'L':
+        return 'gal';
+      case 'gal':
+        return 'L';
+      case 'lbs':
+        return 'kg';
+      case 'kg':
+        return 'lbs';
+      case 'mi':
+        return 'km';
+      case 'km':
+        return mi;
+      default:
+        throw new Error('initUnit invalid');
+    };
   };
 
   this.spellOutUnit = function(unit) {
-    var result;
-    
-    return result;
+    switch (unit) {
+      case 'L':
+        return 'liters';
+      case 'gal':
+        return 'gallons';
+      case 'lbs':
+        return 'pounds';
+      case 'kg':
+        return 'kilograms';
+      case 'mi':
+        return 'miles';
+      case 'km':
+        return 'kilometers';
+      default:
+        throw new Error('unit invalid');
+    };
   };
   
   this.convert = function(initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    var result;
-    
-    return result;
+    switch (initUnit) {
+      case 'L':
+        return initNum / galToL;
+      case 'gal':
+        return initNum * galToL;
+      case 'lbs':
+        return initNum * lbsToKg;
+      case 'kg':
+        return initNum / lbsToKg;
+      case 'mi':
+        return initNum * miToKm;
+      case 'km':
+        return initNum / miToKm;
+      default:
+        throw new Error('initUnit invalid');
+    };
   };
   
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    var result;
-    
-    return result;
+    return `${initNum} ${initUnit} converts to ${returnNum} ${returnUnit}`;
   };
   
 }
