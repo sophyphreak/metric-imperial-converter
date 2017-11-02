1e18fbbf905e99a8915a04e08078e10412dfc091
Andrew Horn <arhorn@smcm.edu>
2017-11-03 06:20:04 +0700

    updated api.js to es6

--- routes/api.js
+++ routes/api.js
@@ -8,20 +8,20 @@
 
 'use strict';
 
-var ConvertHandler = require('../controllers/convertHandler.js');
+const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
   
-  var convertHandler = new ConvertHandler();
+  const convertHandler = new ConvertHandler();
 
   app.route('/api/convert')
     .get(function (req, res){
-      var input = req.query.input;
-      var initNum = convertHandler.getNum(input);
-      var initUnit = convertHandler.getUnit(input);
-      var returnNum = convertHandler.convert(initNum, initUnit);
-      var returnUnit = convertHandler.getReturnUnit(initUnit);
-      var toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+      const input = req.query.input;
+      const initNum = convertHandler.getNum(input);
+      const initUnit = convertHandler.getUnit(input);
+      const returnNum = convertHandler.convert(initNum, initUnit);
+      const returnUnit = convertHandler.getReturnUnit(initUnit);
+      const toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
       
       //res.json
     });
